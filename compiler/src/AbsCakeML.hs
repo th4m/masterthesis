9aa12234214072e37802789c57cebaf796909a4e
th4m <ph.th4m@gmail.com>
2017-02-03 15:54:56 +0100

    Added rest of expressions

--- compiler/src/AbsCakeML.hs
+++ compiler/src/AbsCakeML.hs
@@ -1,34 +1,13 @@
 module AbsCakeML where
 
+import Numeric.Natural
+
 -- | Literal constants
 data Lit
   = IntLit Int
   | CharLit Char
   | StrLit String
   deriving (Eq)
-
--- | Built-in binary operations
-data Opn
-  = Plus
-  | Minus
-  | Times
-  | Divide
-  | Modulo
-
-data Opb
-  = Lt
-  | Gt
-  | LEq
-  | GEq
-
--- | Expressions
-data Exp
-  = Raise Exp
-  | Literal Lit
-  | App Op [Exp]
-  | Var (Id VarN)
-  | Log LOp Exp Exp
-  | If Exp Exp Exp
 
 -- | Identifiers
 data Id a
@@ -69,12 +48,65 @@ data TCtor
   | TC_char
   | TC_string
   | TC_fn
+  deriving (Eq)
+
+-- | Types
+data T
+  = TVar TVarN
+  | TVar_DB Natural
+  | TApp [T] TCtor
+  deriving (Eq)
+
+-- | Patterns
+data Pat
+  = PVar VarN
+  | PLit Lit
+  | PCon (Maybe (Id ConN)) [Pat]
+  | PRef Pat
+  | PTAnnot Pat T
+  deriving (Eq)
+
+-- | Expressions
+data Exp
+  = Raise Exp
+  | Handle Exp [(Pat, Exp)]
+  | Literal Lit
+  | Con (Maybe (Id ConN)) [Exp]
+  | Var (Id VarN)
+  | Fun VarN Exp
+  | App Op [Exp]
+  | Log LOp Exp Exp
+  | If Exp Exp Exp
+  | Mat Exp [(Pat, Exp)]
+  | Let (Maybe VarN) Exp Exp
+  | LetRec [(VarN, VarN, Exp)] Exp
+  | TAnnot Exp T
+  deriving (Eq)
 
 data Op
   = OPN Opn
   | OPB Opb
   | OpApp
+  deriving (Eq)
+
+-- | Built-in binary operations
+data Opn
+  = Plus
+  | Minus
+  | Times
+  | Divide
+  | Modulo
+  deriving (Eq)
+
+data Opb
+  = Lt
+  | Gt
+  | LEq
+  | GEq
+  deriving (Eq)
 
 data LOp
   = And
   | Or
+  deriving (Eq)
+
