1544cd17eef9603be4b9b55115d5d23377da41ac
th4m <ph.th4m@gmail.com>
2017-03-15 14:23:28 +0100

    Created a manual "brute-force" kind of replacer for Let-expressions

--- compiler/src/Compile.hs
+++ compiler/src/Compile.hs
@@ -21,9 +21,49 @@ compile (App op es) =
 compile (Log lop e1 e2) = Log lop (compile e1) (compile e2)
 compile (If e1 e2 e3) = If (compile e1) (compile e2) (compile e3)
 compile (Mat e pes) = Mat (compile e) pes
-compile (Let xo e1 e2) = undefined
+compile (Let xo e1 e2) = case xo of
+  Just n ->
+    replace n e1 e2
+  Nothing ->
+    undefined -- Just do a normal let with e1 and e2?
 compile (LetRec funs e) = undefined
 compile (TAnnot e t) = TAnnot (compile e) t
+
+
+replace :: VarN -> Exp -> Exp -> Exp
+replace vn e (Var n) =
+  case n of
+    Short n ->
+      if vn == n then
+        e
+      else
+        Var (Short n)
+    Long mn n ->
+      undefined -- Should it be possible to replace variables from other modules?
+replace vn e (Raise e') =
+  Raise $ replace vn e e'
+replace vn e (Handle e' pes) =
+  Handle (replace vn e e') pes
+replace vn e (Con cn es) =
+  Con cn $ map (replace vn e) es
+replace vn e (Fun x e') =
+  Fun x $ replace vn e e'
+replace _  _ (Literal l) =
+  Literal l
+replace vn e (App op es) =
+  App op $ map (replace vn e) es
+replace vn e (Log lop e1 e2) =
+  Log lop e1' e2'
+  where [e1', e2'] = map (replace vn e) [e1, e2]
+replace vn e (If e1 e2 e3) =
+  If e1' e2' e3'
+  where [e1', e2', e3'] = map (replace vn e) [e1, e2, e3]
+replace vn e (Mat e' pes) =
+  Mat (replace vn e e') pes
+replace vn e (LetRec funs e') =
+  LetRec funs $ replace vn e e' -- Should it replace in each definition in funs?
+replace vn e (TAnnot e' t) =
+  TAnnot (replace vn e e') t
 
 
 convArith :: Opn -> [Exp] -> Exp
