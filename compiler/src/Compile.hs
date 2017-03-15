fc6c06a078a97305e375f4cfbde0403f1d641c44
th4m <ph.th4m@gmail.com>
2017-03-15 11:49:59 +0100

    Added definitions for most cases

--- compiler/src/Compile.hs
+++ compiler/src/Compile.hs
@@ -5,13 +5,26 @@ import AbsCakeML
 
 
 compile :: Exp -> Exp
+compile (Raise e) = Raise $ compile e
+compile (Handle e pes) = Handle (compile e) pes
+compile (Con cn es) = Con cn $ map compile es
+compile (Var n) = Var n
+compile (Fun x e) = Fun x $ compile e
+compile (Literal l) = Literal l
 compile (App op es) =
   if op == OpApp then
-    undefined
+    App OpApp $ map compile es
   else
     case op of
       OPN opn ->
         convArith opn es
+compile (Log lop e1 e2) = Log lop (compile e1) (compile e2)
+compile (If e1 e2 e3) = If (compile e1) (compile e2) (compile e3)
+compile (Mat e pes) = Mat (compile e) pes
+compile (Let xo e1 e2) = undefined
+compile (LetRec funs e) = undefined
+compile (TAnnot e t) = TAnnot (compile e) t
+
 
 convArith :: Opn -> [Exp] -> Exp
 convArith op [e1, e2]
