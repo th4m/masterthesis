b6358b601f256cac59216a6dbb35d38a3b47fd75
th4m <ph.th4m@gmail.com>
2017-03-16 13:56:29 +0100

    Updated compile a little and added a missed Let-case for replace

--- compiler/src/Compile.hs
+++ compiler/src/Compile.hs
@@ -13,20 +13,27 @@ compile (Fun x e) = Fun x $ compile e
 compile (Literal l) = Literal l
 compile (App op es) =
   if op == OpApp then
-    App OpApp $ map compile es
+    case es of
+      [Fun x e1, e2] ->
+        compile $ replace x e2 e1 -- already handled by evaluate?
+      -- Recursion?
+      _ ->
+        App OpApp $ map compile es
   else
     case op of
       OPN opn ->
         convArith opn es
+      _ ->
+        App op $ map compile es
 compile (Log lop e1 e2) = Log lop (compile e1) (compile e2)
 compile (If e1 e2 e3) = If (compile e1) (compile e2) (compile e3)
 compile (Mat e pes) = Mat (compile e) pes
 compile (Let xo e1 e2) = case xo of
   Just n ->
-    replace n e1 e2
+    replace n e1 $ compile e2
   Nothing ->
     undefined -- Just do a normal let with e1 and e2?
-compile (LetRec funs e) = LetRec funs (compile e) -- Should compile funs too?
+compile (LetRec funs e) = LetRec funs (compile e) -- Should compile funs too? Should use replace?
 compile (TAnnot e t) = TAnnot (compile e) t
 
 
@@ -60,6 +67,9 @@ replace vn e (If e1 e2 e3) =
   where [e1', e2', e3'] = map (replace vn e) [e1, e2, e3]
 replace vn e (Mat e' pes) =
   Mat (replace vn e e') pes
+replace vn e (Let xo e1 e2) =
+  Let xo e1' e2'
+  where [e1', e2'] = map (replace vn e) [e1, e2]
 replace vn e (LetRec funs e') =
   LetRec funs $ replace vn e e' -- Should it replace in each definition in funs?
 replace vn e (TAnnot e' t) =
